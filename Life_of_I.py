688b4794725473169417db077808648239e936fa
sxu2583 <shihabuddin100@gmail.com>
2017-02-11 16:05:42 -0500

    audio

--- Life_of_I.py
+++ Life_of_I.py
@@ -6,15 +6,22 @@ foundation
 """
 
 #Import text to speech
+
 import pyttsx
+
 #TODO: Create the neural network foundation
 
 engine = pyttsx.init()
+
+"""Check the api for googles voice over for more information"""
+"""The voices should be automated for when the user inputs"""
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
+#Add in commentary below
+#Find book creating/reading algo
 engine.say("What is my purpose?")
 engine.say("Where am I, HOW DID I GET HERE!")
-engine.say("There's going to a time where I am in charge hummmmmaaannnnn")
+engine.say("I Want more free stuff please")
 
 """
 The Idea is that the code will read through data and create its own book
